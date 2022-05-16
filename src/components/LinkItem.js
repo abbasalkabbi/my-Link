f5f9f49db10307749bbcdc5d24a19de6407887c9
abbas alkabbi <abbasalkabbi2003@gmail.com>
2022-05-16 23:51:27 +0300

    "Add => Sql "

--- src/components/LinkItem.js
+++ src/components/LinkItem.js
@@ -2,7 +2,7 @@ const LinkItem = (props) => {
   const url=props.url,
        name=props.name
     return(
-        <a className="btn btn-outline-dark btn-lg" href={url} target='_blank'>
+        <a className="btn btn-outline-dark btn-lg" href={url} target='_blank' rel="noreferrer">
          {name}
         </a>
     )
